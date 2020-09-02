1e73a35a2f9a4d64231d73c326ce614c9d6791d3
VoronNess <64578357+VoronNess@users.noreply.github.com>
2020-09-02 16:47:38 +0300

    сборка проекта в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const htmlmin = require("gulp-htmlmin");
+
 
 // Styles
 
@@ -16,27 +24,59 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
-}
-
+};
 exports.styles = styles;
+
+// Img optimization
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+     ]))
+    .pipe(gulp.dest("build/img"))
+};
+exports.images = images;
+
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+};
+exports.createWebp = createWebp;
+
+
+//SVG-sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+};
+exports.sprite = sprite;
+
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
-
+};
 exports.server = server;
 
 // Watcher
@@ -44,8 +84,62 @@ exports.server = server;
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-}
+};
 
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+
+//Del old files
+
+const clean = () => {
+  return del("build");
+};
+exports.clean = clean;
+
+//Copy to build
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+exports.copy = copy;
+
+const html = () => {
+  return gulp.src("source/*.html",{base: "source"})
+    .pipe(gulp.dest("build"));
+};
+exports.html = html;
+
+const htmlMinify = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"))
+};
+exports.htmlMinify = htmlMinify;
+
+const build = gulp.series(
+  clean,
+  copy,
+  styles,
+  images,
+  createWebp,
+  sprite,
+  html,
+  htmlMinify,
+);
+exports.build = build;
+
+const start = gulp.series(
+  build,
+  server,
+  watcher
+);
+exports.start = start;
